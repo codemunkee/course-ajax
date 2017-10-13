6785a87a27d0d80036ddf1b59a25e474fa08a0dd
Russ Nealis <codemunkee@gmail.com>
2017-10-13 09:18:10 -0700

    added some code to show articles on nytimes

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,9 +4,29 @@
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
+    function addArticles() {
+        const jsonResp = JSON.parse(this.response);
+        let articles = '';
+        console.log(jsonResp['response']['docs']);
+        for (const article of jsonResp['response']['docs']) {
+            articles += `<article>
+                           <h1><a href="${article['web_url']}">${article['headline']['main']}</a></h1>
+                           <p>${article['snippet']}</p>
+                         </article>`
+        }
+        responseContainer.innerHTML = articles;
+    }
+
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
+
+        const articleRequest = new XMLHttpRequest();
+
+        const apiKey = '9bfb1da84c8444fe87156f36e38db2ae';
+        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=${apiKey}`);
+        articleRequest.onload = addArticles;
+        articleRequest.send();
     });
 })();
